9dcc9a885f25b403ca1aa9cb573e40d1e71b97cf
nctuan2013 <54241848+nctuan2013@users.noreply.github.com>
2023-10-07 12:42:57 +0700

    Tạo Project Hello world

---
+++ HelloWold/HelloWold/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWold
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, lop SOF205_01_KieuMai!");
+        }
+    }
+}
